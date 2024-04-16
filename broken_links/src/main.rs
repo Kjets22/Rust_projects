3a80aed068f87bca6c6e8a7453e7ccd46d39495f
Krish Jetly <Kjetly2003@gmail.com>
2024-04-16 14:40:28 -0400

    updated broken links works now

--- broken_links/src/main.rs
+++ broken_links/src/main.rs
@@ -1,9 +1,8 @@
+use clap::error::Result;
 use clap::Parser;
 use linkify::{LinkFinder, Links};
-use std::collections::LinkedList;
-use std::fs::{self, read_to_string, ReadDir};
+use std::fs::{self, read_to_string};
 use std::path::{Path, PathBuf};
-use std::{io, path};
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Checks for broken links in a directory.", long_about = None)]
@@ -15,27 +14,40 @@ struct Args {
 
 //so what this does it takes a derectory and then opens its into each of its files
 //then it gets the contents of each file and sends it to a method
-fn directory_to_file_action<F>(files: &Path, method: F) {
-    //here what i am doing taking a method as a parameter
-    // where
-    // // F: FnMut(Path){
-    // let files = fs::read_dir(files)?;
-    // for file in files {
-    //     let file = file?; //this make it so it is dirEntry and no longer a result enum
-    //     let file = file.path();
-    //     let content = read_to_string(file)?;
-
-    //}
-    // }
+// fn directory_to_file_action<F>(files: &Path, method: F) -> Result<_, _>
+// //here what i am doing taking a method as a parameter
+// where
+//     F: FnMut(Path),
+// {
+//     //let files = fs::read_dir(files)?;
+//     //result<ReadDir,Error>
+//     //it return result with just the self without the error
+//     //if files==eror
+//     // return files
+//     for file in files {
+//         let file = file?; //this make it so it is dirEntry and no longer a result enum
+//         let file = file.path();
+//         let content = read_to_string(file)?;
+//     }
+// }
+fn directory_to_file_action(files: &Path) -> Result<()> {
+    println!("in direcotry_file");
+    let files = fs::read_dir(files)?;
+    let _useless: Vec<_> = files
+        .map(|file| find_links(read_to_string(file.unwrap().path()).unwrap()))
+        .collect();
+    Ok(())
 }
 
 fn find_links(content: String) {
+    println!("in find links");
     let finder = LinkFinder::new();
     let links = finder.links(&content);
     print_brokenlinks(links)
 }
 
 fn print_brokenlinks(links: Links) {
+    println!("in print_brokenlinks");
     for link in links {
         let link = link.as_str();
         match reqwest::blocking::get(link) {
@@ -52,7 +64,7 @@ fn print_brokenlinks(links: Links) {
 fn main() {
     let cli = Args::parse();
     if let Some(path) = cli.broken_link_path.as_deref() {
-        directory_to_file_action(path, find_links);
+        directory_to_file_action(path);
     }
     //let args = Args::parse();
     //if let Some(broken_link_path) = args.broken_link_path.as_deref() {
