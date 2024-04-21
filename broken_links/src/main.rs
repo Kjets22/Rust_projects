225510dec113e4bd71f21c68f9bf89fd55751639
Krish Jetly <Kjetly2003@gmail.com>
2024-04-21 14:56:03 -0400

    got the brokenlinks to work with linkify

--- broken_links/src/main.rs
+++ broken_links/src/main.rs
@@ -1,48 +1,62 @@
 use clap::error::Result;
 use clap::Parser;
+use lazy_static::lazy_static;
 use linkify::{LinkFinder, Links};
 use std::{
     fs::{self, read_to_string},
     path::{Path, PathBuf},
 };
+lazy_static! {
+    static ref FINDER: LinkFinder = LinkFinder::new();
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Checks for broken links in a directory.", long_about = None)]
+/// CLI
 struct Args {
     /// Give a folder with broken links path
     #[arg(short, long)]
     broken_link_path: Option<PathBuf>,
 }
 
-//takes a direcotry and opens it converts to content in the files and runs a method
-fn directory_to_file_action<F>(files: &path, method: F) -> Result<()>
+/// takes a direcotry and opens it converts to content in the files and runs a method
+fn directory_to_file_action<F>(files: &Path, method: &F) -> Result<()>
 where
     F: Fn(String),
 {
     let files = fs::read_dir(files)?;
-    files
-        .map(|file| method(read_to_string(file.unwrap().path()).unwrap()))
-        .collect::<Vec<_>>();
+    for file in files {
+        let file = file.unwrap().path();
+        match read_to_string(&file) {
+            Ok(content) => {
+                method(content);
+            }
+            Err(_) => {
+                let _ = directory_to_file_action(&file, method);
+            }
+        }
+    }
     Ok(())
 }
 
-//find the links through the linkify crate
+/// find the links through the linkify crate
 fn find_links(content: String) {
-    println!("in find links");
-    let finder = LinkFinder::new();
-    let mut links = finder.links(&content);
-    let trying = links.nth(0);
+    let links = FINDER.links(&content);
     print_brokenlinks(links)
 }
 
-//takes the links and sees if they actualy go anywhere
+/// takes the links and sees if they actualy go anywhere
 fn print_brokenlinks(links: Links) {
-    println!("in print_brokenlinks");
     for link in links {
         let link = link.as_str();
         match reqwest::blocking::get(link) {
-            Ok(_l) => {
-                continue;
+            Ok(result) => {
+                if result.status().is_success() {
+                    // if finds url and not 404 anything in the 200s
+                } else {
+                    // if its a 404 or anything else
+                    println!("{}\n", link);
+                }
             }
             Err(_e) => {
                 println!("{}\n", link);
@@ -51,10 +65,10 @@ fn print_brokenlinks(links: Links) {
     }
 }
 
-//main method
+/// main method
 fn main() {
     let cli = Args::parse();
     if let Some(path) = cli.broken_link_path.as_deref() {
-        directory_to_file_action(path, find_links);
+        let _ = directory_to_file_action(path, &find_links);
     }
 }
